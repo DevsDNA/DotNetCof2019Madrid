c6de2c9e8cc6afb72a46137f1e4cf60ed30b0069
Josué Yeray Julián Ferreiro <jyeray@devsdna.com>
2019-10-21 16:53:25 +0100

    Added IAsyncEnumerable code for collection view

--- DotNetConf2019/DotNetConf2019/DotNetConf2019/Features/CollectionSample/CollectionSampleViewModel.cs
+++ DotNetConf2019/DotNetConf2019/DotNetConf2019/Features/CollectionSample/CollectionSampleViewModel.cs
@@ -5,16 +5,18 @@
     using System.Threading.Tasks;
     using System.Windows.Input;
     using Xamarin.Forms;
+    using System.Collections.Generic;
+    using System.Threading;
 
     public class CollectionSampleViewModel : BaseViewModel
     {
         private ObservableCollection<string> items;
         private int index = 1;
+        private bool loading = false;
         
         public CollectionSampleViewModel()
         {
             items = new ObservableCollection<string>();
-            AddItems(100);
             RefreshCommand = new Command(Refresh);
         }
 
@@ -30,21 +32,64 @@
 
         public ICommand RefreshCommand { get; set; }
 
-        private async void Refresh(object obj)
+        public bool Loading
         {
-            await Task.Delay(1000);
-            AddItems(100);
+            get => loading;
+            set
+            {
+                loading = value;
+                OnPropertyChanged();
+            }
         }
 
-        private void AddItems(int numberOfItems)
+        private async void Refresh(object obj)
         {
-            if (index >= 300)
-                return;
-
-            for (int i = 0; i < numberOfItems; i++)
+            if (!loading)
             {
-                Items.Add($"Item {index}");
+                Loading = true;
+                foreach (string item in await AddItems(50))
+                {
+                    if (Items.Count % 2 == 0)
+                        Items.Add($"{item} es impar");
+                    else
+                        Items.Add($"{item} es par");
+                }
+
+
+                //await foreach (string item in AddItemsAsync(50).ConfigureAwait(false))
+                //{
+                //    if (Items.Count % 2 == 0)
+                //        Items.Add($"{item} es impar");
+                //    else
+                //        Items.Add($"{item} es par");
+                //}
+
+                Loading = false;
+            }
+        }
+
+        private async Task<IEnumerable<string>> AddItems(int numberOfItems)
+        {
+            var newItems = new List<string>();
+            index = 1;
+            for (int i = 1; i <= numberOfItems; i++)
+            {
+                await Task.Delay(100);
+                newItems.Add($"Item {index}");
                 index++;
+            }
+
+            return newItems;
+        }
+
+        private async IAsyncEnumerable<string> AddItemsAsync(int numberOfItems)
+        {
+            index = Items.Count;
+            for (int i = 1; i <= numberOfItems; i++)
+            {
+                await Task.Delay(100);
+                index++;
+                yield return $"Item {index}";
             }
         }
     }
